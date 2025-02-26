09a0290c17cfedaab4b5ed78f752409e40584cc8
Dav50505 <87789270+Dav50505@users.noreply.github.com>
2025-02-25 18:15:07 -0800

    data.js

--- src/data.js
+++ src/data.js
@@ -2,21 +2,63 @@ const projects = [
   {
     title: 'Cart Project',
     url: 'https://checkout-cart-pro.netlify.app/',
-    image: './assets/screenshots (444).png',
+    image: '/src/assets/cart.png',
   },
   {
     title: 'Tours',
     url: 'https://tours-project-1.netlify.app/',
-    image: './assets/tours.png',
+    image: '/src/assets/tours.png',
   },
   {
     title: 'Interns',
     url: 'https://active-button-project.netlify.app/',
-    image: './assets/screenshots (445).png',
+    image: '/src/assets/interns.png',
   },
   {
     title: 'questions',
     url: 'https://questions-project-1.netlify.app/',
-    image: './assets/questions.png',
+    image: '/src/assets/questions.png',
+  },
+  {
+    title: 'colors',
+    url: 'https://questions-project-1.netlify.app/',
+    image: '/src/assets/colors.png',
+  },
+  {
+    title: 'Cute Pictures',
+    url: 'https://questions-project-1.netlify.app/',
+    image: '/src/assets/cute-images.png',
+  },
+  {
+    title: 'Drinks',
+    url: 'https://questions-project-1.netlify.app/',
+    image: '/src/assets/Drinks.png',
+  },
+  {
+    title: 'Dropdown',
+    url: 'https://questions-project-1.netlify.app/',
+    image: '/src/assets/DropDown.png',
+  },
+  {
+    title: 'Menu',
+    url: 'https://questions-project-1.netlify.app/',
+    image: '/src/assets/Menu.png',
+  },
+  {
+    title: 'Navbar',
+    url: 'https://questions-project-1.netlify.app/',
+    image: '/src/assets/navbar.png',
+  },
+  {
+    title: 'Paragraph Generator',
+    url: 'https://questions-project-1.netlify.app/',
+    image: '/src/assets/Paragraph-gen.png',
+  },
+  {
+    title: 'Sidebar-Modal',
+    url: 'https://questions-project-1.netlify.app/',
+    image: '/src/assets/Sidebar-modal.png',
   },
 ];
+
+export default projects;
